07ad94eea0164544a906f53605fbdce5e06b9ae0
KY <chua.kayi@yahoo.com.sg>
2014-07-03 11:36:24 +0800

    Includes special programme choices e.g. minor, ddp

--- js/reviewscript.js
+++ js/reviewscript.js
@@ -12,29 +12,88 @@ $(document).ready(function(){
             }*/
         if ($("select#fac option:selected").val() == "FoS") {
             $("select#maj option").remove();
-            $("select#maj").append("<option value=0> Select Major </option>");
-            $("select#maj").append("<option value=1> Applied Mathematics </option>");
-            $("select#maj").append("<option value=2> Chemistry </option>");
-            $("select#maj").append("<option value=3> Computational Biology </option>");
-            $("select#maj").append("<option value=4> Food Science and Technology</option>");
-            $("select#maj").append("<option value=5> Life Sciences </option>");
-            $("select#maj").append("<option value=6> Mathematics </option>");
-            $("select#maj").append("<option value=7> Pharmacy </option>");
-            $("select#maj").append("<option value=8> Physics </option>");
-            $("select#maj").append("<option value=9> Quantitative Finance</option>");
-            $("select#maj").append("<option value=10> Statistics </option>");
+            $("select#maj").append("<option value=> Select Major </option>");
+            $("select#maj").append("<option value='Applied Mathematics'> Applied Mathematics </option>");
+            $("select#maj").append("<option value='Chemistry'> Chemistry </option>");
+            $("select#maj").append("<option value='Computational Biology'> Computational Biology </option>");
+            $("select#maj").append("<option value='Food Science and Technology'> Food Science and Technology</option>");
+            $("select#maj").append("<option value='Life Sciences'> Life Sciences </option>");
+            $("select#maj").append("<option value='Mathematics'> Mathematics </option>");
+            $("select#maj").append("<option value='Pharmacy'> Pharmacy </option>");
+            $("select#maj").append("<option value='Physics'> Physics </option>");
+            $("select#maj").append("<option value='Quantitative Finance'> Quantitative Finance</option>");
+            $("select#maj").append("<option value='Statistics'> Statistics </option>");
         } else if ($("select#fac option:selected").val() == "SoC") {
             $("select#maj option").remove();
-            $("select#maj").append("<option value=0> Select Major </option>");
-            $("select#maj").append("<option value=1> Business Analytics </option>");
-            $("select#maj").append("<option value=2> Computational Biology</option>");
-            $("select#maj").append("<option value=3> Computer Engineering</option>");
-            $("select#maj").append("<option value=4> Computer Science </option>");
-            $("select#maj").append("<option value=5> Electronic Commerce</option>");
-            $("select#maj").append("<option value=6> Information Systems </option>");
+            $("select#maj").append("<option value=> Select Major </option>");
+            $("select#maj").append("<option value='Business Analytics'> Business Analytics </option>");
+            $("select#maj").append("<option value='Computational Biology'> Computational Biology</option>");
+            $("select#maj").append("<option value='Computer Engineering'> Computer Engineering</option>");
+            $("select#maj").append("<option value='Computer Science'> Computer Science </option>");
+            $("select#maj").append("<option value='Electronic Commerce'> Electronic Commerce</option>");
+            $("select#maj").append("<option value='Information Systems'> Information Systems </option>");
         } else {
             $("select#maj option").remove();
-            $("select#maj").append("<option value=0> Select Major </option>");
+            $("select#maj").append("<option value=> Select Major </option>");
+        }
+            
+    })
+
+	$("select#specprog").change(function() {
+		if ($("select#specprog option:selected").val() == "NIL") {
+			$("select#fac2 option").remove();
+			$("select#fac2").append("<option value='NIL'> NIL </option>");
+			$("select#fac2").val("NIL");
+			$("select#maj2 option").remove();
+			$("select#maj2").append("<option value='NIL'> NIL </option>");
+			$("select#maj2").val("NIL");
+		} else if ($("select#specprog option:selected").val() !== "") {
+			$("select#maj2 option").remove();
+			$("select#maj2").append("<option value=> Select Major/Minor </option>");
+			$("select#fac2 option").remove();
+			$("select#fac2").append("<option value=> Select Faculty </option>");
+			$("select#fac2").append("<option value='FoS'> Faculty of Science </option>");
+			$("select#fac2").append("<option value='SoC'> School of Computing </option>");
+
+		} else {
+			$("select#maj2 option").remove();
+			$("select#maj2").append("<option value=> Select Major/Minor </option>");
+			$("select#fac2 option").remove();
+			$("select#fac2").append("<option value=> Select Faculty </option>");
+		}
+	})
+
+	$("select#fac2").change(function() {
+            /*if $("select#fac option:selected").text() == "Faculty of Science") {
+              $("select#maj").append("<option value=0> Physics </option>");
+            } else {
+              $("select#maj").append("<option value=0> Computer science </option>");
+            }*/
+        if ($("select#fac2 option:selected").val() == "FoS") {
+            $("select#maj2 option").remove();
+			$("select#maj2").append("<option value=> Select Major/Minor </option>");
+            $("select#maj2").append("<option value='Applied Mathematics'> Applied Mathematics </option>");
+            $("select#maj2").append("<option value='Chemistry'> Chemistry </option>");
+            $("select#maj2").append("<option value='Computational Biology'> Computational Biology </option>");
+            $("select#maj2").append("<option value='Food Science and Technology'> Food Science and Technology</option>");
+            $("select#maj2").append("<option value='Life Sciences'> Life Sciences </option>");
+            $("select#maj2").append("<option value='Mathematics'> Mathematics </option>");
+            $("select#maj2").append("<option value='Pharmacy'> Pharmacy </option>");
+            $("select#maj2").append("<option value='Physics'> Physics </option>");
+            $("select#maj2").append("<option value='Quantitative Finance'> Quantitative Finance</option>");
+            $("select#maj2").append("<option value='Statistics'> Statistics </option>");
+        } else if ($("select#fac2 option:selected").val() == "SoC") {
+            $("select#maj2 option").remove();
+            $("select#maj2").append("<option value=> Select Major/Minor </option>");
+            $("select#maj2").append("<option value='Business Analytics'> Business Analytics </option>");
+            $("select#maj2").append("<option value='Computational Biology'> Computational Biology</option>");
+            $("select#maj2").append("<option value='Computer Engineering'> Computer Engineering</option>");
+            $("select#maj2").append("<option value='Computer Science'> Computer Science </option>");
+            $("select#maj2").append("<option value='Electronic Commerce'> Electronic Commerce</option>");
+            $("select#maj2").append("<option value='Information Systems'> Information Systems </option>");
+        } else {
+            $("select#maj2 option").remove();
+            $("select#maj2").append("<option value=> Select Major/Minor </option>");
         }
             
     })
