1ed9e73d6fa8eec21a71594e0c0e474f075a4395
Yap Ying Ying <yapyingying94@gmail.com>
2014-07-04 15:11:18 +0800

    created admin. Did not deploy yet
    Created a new page for login as nus students or admin users. Cos can't figure out how to get admin login as nus account. for now only can google account. Admin login is for both of us only, those with permission to access the administration console

--- login.py
+++ login.py
@@ -5,11 +5,21 @@ from google.appengine.api import users
 
 class LoginHandler(webapp2.RequestHandler):
   def get(self):
-  	url = self.request.get('continue_url')
-  	if url=="" or url == None:
-	    self.redirect(users.create_login_url('/', None, federated_identity='https://openid.nus.edu.sg/'))
-	else:
-		self.redirect(users.create_login_url(url, None, federated_identity='https://openid.nus.edu.sg/'))
+    url = self.request.get('continue_url')
+    if url=="" or url==None:
+      continue_url = '/'
+    else:
+      continue_url = url
+    openid_url = self.request.get('openid_url')
+    if openid_url:
+      if openid_url == 'NUS':
+        self.redirect(users.create_login_url(continue_url, None, federated_identity='https://openid.nus.edu.sg/'))
+      elif openid_url == 'google':
+      	self.redirect(users.create_login_url(continue_url, None ,federated_identity='https://www.google.com/accounts/o8/id'))
+      else:
+      	self.redirect(users.create_login_url(continue_url, None, federated_identity='https://openid.nus.edu.sg/'))
+    else:    
+      self.redirect(users.create_login_url(continue_url, None, federated_identity='https://openid.nus.edu.sg/'))
 
 app = webapp2.WSGIApplication([('/_ah/login_required', LoginHandler)],
                               debug=True)
