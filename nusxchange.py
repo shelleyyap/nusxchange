5785e8988d75995ae461f31c477f307eae7b3647
Yap Ying Ying <yapyingying94@gmail.com>
2014-07-14 14:30:04 +0800

    Added edit review and edit uni

--- nusxchange.py
+++ nusxchange.py
@@ -700,6 +700,58 @@ class EditReview(webapp2.RequestHandler):
 
     self.redirect("/university?school=" + review_school)
     
+class EditUni(webapp2.RequestHandler):
+  def get(self):
+      countries_list={'australia': "Australia", 'canada': "Canada", 'china': "China", 'germany': "Germany", 'hongkong': "Hong Kong"}
+      target = self.request.get('school')
+      query = School.query(School.school_name_short.IN([target])).get()
+      template_values = {
+        'text': 'Logout',
+        'url': users.create_logout_url('/countries'),
+        'school': query
+      }
+      template = jinja_environment.get_template('edituni.html')
+      self.response.out.write(template.render(template_values))
+  def post(self):
+      countries_list={'australia': "Australia", 'canada': "Canada", 'china': "China", 'germany': "Germany", 'hongkong': "Hong Kong"}
+      target = self.request.get('school')
+      query = School.query(School.school_name_short.IN([target])).get()
+      def sanitizeHtml(value, base_url=None):
+        rjs = r'[\s]*(&#x.{1,7})?'.join(list('javascript:'))
+        rvb = r'[\s]*(&#x.{1,7})?'.join(list('vbscript:'))
+        re_scripts = re.compile('(%s)|(%s)' % (rjs, rvb), re.IGNORECASE)
+        validTags = 'p i strong b u a h1 h2 h3 pre br img'.split()
+        validAttrs = 'href src width height'.split()
+        urlAttrs = 'href src'.split() # Attributes which should have a URL
+        soup = BeautifulSoup(value)
+        for comment in soup.findAll(text=lambda text: isinstance(text, Comment)):
+          # Get rid of comments
+          comment.extract()
+        for tag in soup.findAll(True):
+          if tag.name not in validTags:
+            tag.hidden = True
+          attrs = tag.attrs
+          tag.attrs = []
+          for attr, val in attrs:
+            if attr in validAttrs:
+              val = re_scripts.sub('', val) # Remove scripts (vbs & js)
+              if attr in urlAttrs:
+                val = urljoin(base_url, val) # Calculate the absolute url
+              tag.attrs.append((attr, val))
+        return soup.renderContents().decode('utf8')
+
+      query.school_name=self.request.get('name')
+      query.country=self.request.get('country')
+      query.state=self.request.get('state')
+      query.exchange_type=self.request.get('exchange_type')
+      query.academic_calendar=self.request.get('calendar')
+      query.recommended_fac=self.request.get_all('faculty')
+      query.mod_offered=self.request.get('modules')
+      query.content=sanitizeHtml(self.request.get('abtschool'))  
+
+      query.put()
+      self.redirect("/countries")
+    
 app = webapp2.WSGIApplication([('/', MainPage),
                                 ('/login', Login),
                                 ('/about', About),
@@ -715,5 +767,6 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                 ('/search', Search),
                                 ('/modulemappings', ModuleMappings),
                                 ('/check_name', CheckName),
-                                ('/editreview', EditReview)],
+                                ('/editreview', EditReview),
+                                ('/edituni', EditUni)],
                               debug=True)
