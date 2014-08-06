f202ef30b206b7fcded3ddaf51928eb62d98de0b
KY <chua.kayi@yahoo.com.sg>
2014-08-06 17:01:31 +0800

    Supports all majors

--- js/reviewscript.js
+++ js/reviewscript.js
@@ -34,6 +34,69 @@ $(document).ready(function(){
             $("select#maj").append("<option value='Computer Science'> Computer Science </option>");
             $("select#maj").append("<option value='Electronic Commerce'> Electronic Commerce</option>");
             $("select#maj").append("<option value='Information Systems'> Information Systems </option>");
+        } else if ($("select#fac option:selected").val() == "FASS") {
+            $("select#maj option").remove();
+            $("select#maj").append("<option value=> Select Major </option>");
+            $("select#maj").append("<option value='Chinese Language'> Chinese Language </option>");
+            $("select#maj").append("<option value='Chinese Studies'> Chinese Studies </option>");
+            $("select#maj").append("<option value='Communications and New Media'> Communications and New Media </option>");
+            $("select#maj").append("<option value='Economics'> Economics </option>");
+            $("select#maj").append("<option value='English Language'> English Language </option>");
+            $("select#maj").append("<option value='English Literature'> English Literature </option>");
+            $("select#maj").append("<option value='European Studies'> European Studies </option>");
+            $("select#maj").append("<option value='Geography'> Geography </option>");
+            $("select#maj").append("<option value='History'> History </option>");
+            $("select#maj").append("<option value='Japanese Studies'> Japanese Studies </option>");
+            $("select#maj").append("<option value='Malay Studies'> Malay Studies </option>");
+            $("select#maj").append("<option value='Philosophy'> Philosophy </option>");
+            $("select#maj").append("<option value='Political Science'> Political Science </option>");
+            $("select#maj").append("<option value='Psychology'> Psychology </option>");
+            $("select#maj").append("<option value='Social Work'> Social Work </option>");
+            $("select#maj").append("<option value='Sociology'> Sociology </option>");
+            $("select#maj").append("<option value='South Asian Studies'> South Asian Studies </option>");
+            $("select#maj").append("<option value='Southeast Asian Studies'> Southeast Asian Studies </option>");
+            $("select#maj").append("<option value='Theatre Studies'> Theatre Studies </option>");
+        } else if ($("select#fac option:selected").val() == "Biz") {
+            $("select#maj option").remove();
+            $("select#maj").append("<option value=> Select Major </option>");
+            $("select#maj").append("<option value='Business Administration'> Business Administration </option>");
+            $("select#maj").append("<option value='Business Administration (Accountancy)'> Business Administration (Accountancy) </option>");
+        } else if ($("select#fac option:selected").val() == "FoD") {
+            $("select#maj option").remove();
+            $("select#maj").append("<option value='Dentistry'> Dentistry </option>");
+            $("select#maj").val("Dentistry");
+        } else if ($("select#fac option:selected").val() == "SDE") {
+            $("select#maj option").remove();
+            $("select#maj").append("<option value=> Select Major </option>");
+            $("select#maj").append("<option value='Architecture'> Architecture </option>");
+            $("select#maj").append("<option value='Industrial Design'> Industrial Design </option>");
+            $("select#maj").append("<option value='Project and Facilities Management'> Project and Facilities Management </option>");
+            $("select#maj").append("<option value='Real Estate'> Real Estate </option>");
+        } else if ($("select#fac option:selected").val() == "FoE") {
+            $("select#maj option").remove();
+            $("select#maj").append("<option value=> Select Major </option>");
+            $("select#maj").append("<option value='Biomedical Engineering'> Biomedical Engineering </option>");
+            $("select#maj").append("<option value='Chemical & Biomolecular Engineering'> Chemical & Biomolecular Engineering </option>");
+            $("select#maj").append("<option value='Civil & Environmental Engineering'> Civil & Environmental Engineering </option>");
+            $("select#maj").append("<option value='Electrical & Computer Engineering'> Electrical & Computer Engineering </option>");
+            $("select#maj").append("<option value='Engineering & Technology Management'> Engineering & Technology Management </option>");
+            $("select#maj").append("<option value='Industrial & Systems Engineering'> Industrial & Systems Engineering </option>");
+            $("select#maj").append("<option value='Materials Science & Engineering'> Materials Science & Engineering </option>");
+            $("select#maj").append("<option value='Mechanical Engineering'> Mechanical Engineering </option>");
+        } else if ($("select#fac option:selected").val() == "Law") {
+            $("select#maj option").remove();
+            $("select#maj").append("<option value='Law'> Law </option>");
+            $("select#maj").val("Law");
+        } else if ($("select#fac option:selected").val() == "Med") {
+            $("select#maj option").remove();
+            $("select#maj").append("<option value=> Select Major </option>");
+            $("select#maj").append("<option value='Medicine'> Medicine </option>");
+            $("select#maj").append("<option value='Nursing'> Nursing </option>");
+        } else if ($("select#fac option:selected").val() == "YSTCM") {
+            $("select#maj option").remove();
+            $("select#maj").append("<option value=> Select Major </option>");
+            $("select#maj").append("<option value='Performance'> Performance </option>");
+            $("select#maj").append("<option value='Composition'> Composition </option>");
         } else {
             $("select#maj option").remove();
             $("select#maj").append("<option value=> Select Major </option>");
@@ -61,9 +124,16 @@ $(document).ready(function(){
 			$("select#maj2").append("<option value=> Select Major/Minor </option>");
 			$("select#fac2 option").remove();
 			$("select#fac2").append("<option value=> Select Faculty </option>");
-			$("select#fac2").append("<option value='FoS'> Faculty of Science </option>");
+            $("select#fac2").append("<option value='FASS'> Faculty of Arts and Social Sciences </option>");
+            $("select#fac2").append("<option value='Biz'> Business School </option>");
 			$("select#fac2").append("<option value='SoC'> School of Computing </option>");
-
+            $("select#fac2").append("<option value='FoD'> Faculty of Dentistry </option>");
+            $("select#fac2").append("<option value='SDE'> School of Design and Environment</option>");
+            $("select#fac2").append("<option value='FoE'> Faculty of Engineering </option>");
+            $("select#fac2").append("<option value='Law'> Faculty of Law </option>");
+            $("select#fac2").append("<option value='Med'> Yong Loo Lin School of Medicine </option>");
+            $("select#fac2").append("<option value='YSTCM'> Yong Siew Toh Conservatory of Music </option>");
+            $("select#fac2").append("<option value='FoS'> Faculty of Science </option>");
 		} else {
 			$("select#maj2 option").remove();
 			$("select#maj2").append("<option value=> Select Major/Minor </option>");
@@ -73,14 +143,9 @@ $(document).ready(function(){
 	})
 
 	$("select#fac2").change(function() {
-            /*if $("select#fac option:selected").text() == "Faculty of Science") {
-              $("select#maj").append("<option value=0> Physics </option>");
-            } else {
-              $("select#maj").append("<option value=0> Computer science </option>");
-            }*/
         if ($("select#fac2 option:selected").val() == "FoS") {
             $("select#maj2 option").remove();
-			$("select#maj2").append("<option value=> Select Major/Minor </option>");
+            $("select#maj2").append("<option value=> Select Major </option>");
             $("select#maj2").append("<option value='Applied Mathematics'> Applied Mathematics </option>");
             $("select#maj2").append("<option value='Chemistry'> Chemistry </option>");
             $("select#maj2").append("<option value='Computational Biology'> Computational Biology </option>");
@@ -93,16 +158,79 @@ $(document).ready(function(){
             $("select#maj2").append("<option value='Statistics'> Statistics </option>");
         } else if ($("select#fac2 option:selected").val() == "SoC") {
             $("select#maj2 option").remove();
-            $("select#maj2").append("<option value=> Select Major/Minor </option>");
+            $("select#maj2").append("<option value=> Select Major </option>");
             $("select#maj2").append("<option value='Business Analytics'> Business Analytics </option>");
             $("select#maj2").append("<option value='Computational Biology'> Computational Biology</option>");
             $("select#maj2").append("<option value='Computer Engineering'> Computer Engineering</option>");
             $("select#maj2").append("<option value='Computer Science'> Computer Science </option>");
             $("select#maj2").append("<option value='Electronic Commerce'> Electronic Commerce</option>");
             $("select#maj2").append("<option value='Information Systems'> Information Systems </option>");
+        } else if ($("select#fac2 option:selected").val() == "FASS") {
+            $("select#maj2 option").remove();
+            $("select#maj2").append("<option value=> Select Major </option>");
+            $("select#maj2").append("<option value='Chinese Language'> Chinese Language </option>");
+            $("select#maj2").append("<option value='Chinese Studies'> Chinese Studies </option>");
+            $("select#maj2").append("<option value='Communications and New Media'> Communications and New Media </option>");
+            $("select#maj2").append("<option value='Economics'> Economics </option>");
+            $("select#maj2").append("<option value='English Language'> English Language </option>");
+            $("select#maj2").append("<option value='English Literature'> English Literature </option>");
+            $("select#maj2").append("<option value='European Studies'> European Studies </option>");
+            $("select#maj2").append("<option value='Geography'> Geography </option>");
+            $("select#maj2").append("<option value='History'> History </option>");
+            $("select#maj2").append("<option value='Japanese Studies'> Japanese Studies </option>");
+            $("select#maj2").append("<option value='Malay Studies'> Malay Studies </option>");
+            $("select#maj2").append("<option value='Philosophy'> Philosophy </option>");
+            $("select#maj2").append("<option value='Political Science'> Political Science </option>");
+            $("select#maj2").append("<option value='Psychology'> Psychology </option>");
+            $("select#maj2").append("<option value='Social Work'> Social Work </option>");
+            $("select#maj2").append("<option value='Sociology'> Sociology </option>");
+            $("select#maj2").append("<option value='South Asian Studies'> South Asian Studies </option>");
+            $("select#maj2").append("<option value='Southeast Asian Studies'> Southeast Asian Studies </option>");
+            $("select#maj2").append("<option value='Theatre Studies'> Theatre Studies </option>");
+        } else if ($("select#fac2 option:selected").val() == "Biz") {
+            $("select#maj2 option").remove();
+            $("select#maj2").append("<option value=> Select Major </option>");
+            $("select#maj2").append("<option value='Business Administration'> Business Administration </option>");
+            $("select#maj2").append("<option value='Business Administration (Accountancy)'> Business Administration (Accountancy) </option>");
+        } else if ($("select#fac2 option:selected").val() == "FoD") {
+            $("select#maj2 option").remove();
+            $("select#maj2").append("<option value='Dentistry'> Dentistry </option>");
+            $("select#maj2").val("Dentistry");
+        } else if ($("select#fac2 option:selected").val() == "SDE") {
+            $("select#maj2 option").remove();
+            $("select#maj2").append("<option value=> Select Major </option>");
+            $("select#maj2").append("<option value='Architecture'> Architecture </option>");
+            $("select#maj2").append("<option value='Industrial Design'> Industrial Design </option>");
+            $("select#maj2").append("<option value='Project and Facilities Management'> Project and Facilities Management </option>");
+            $("select#maj2").append("<option value='Real Estate'> Real Estate </option>");
+        } else if ($("select#fac2 option:selected").val() == "FoE") {
+            $("select#maj2 option").remove();
+            $("select#maj2").append("<option value=> Select Major </option>");
+            $("select#maj2").append("<option value='Biomedical Engineering'> Biomedical Engineering </option>");
+            $("select#maj2").append("<option value='Chemical & Biomolecular Engineering'> Chemical & Biomolecular Engineering </option>");
+            $("select#maj2").append("<option value='Civil & Environmental Engineering'> Civil & Environmental Engineering </option>");
+            $("select#maj2").append("<option value='Electrical & Computer Engineering'> Electrical & Computer Engineering </option>");
+            $("select#maj2").append("<option value='Engineering & Technology Management'> Engineering & Technology Management </option>");
+            $("select#maj2").append("<option value='Industrial & Systems Engineering'> Industrial & Systems Engineering </option>");
+            $("select#maj2").append("<option value='Materials Science & Engineering'> Materials Science & Engineering </option>");
+            $("select#maj2").append("<option value='Mechanical Engineering'> Mechanical Engineering </option>");
+        } else if ($("select#fac2 option:selected").val() == "Law") {
+            $("select#maj2 option").remove();
+            $("select#maj2").append("<option value='Law'> Law </option>");
+            $("select#maj2").val("Law");
+        } else if ($("select#fac2 option:selected").val() == "Med") {
+            $("select#maj2 option").remove();
+            $("select#maj2").append("<option value=> Select Major </option>");
+            $("select#maj2").append("<option value='Medicine'> Medicine </option>");
+            $("select#maj2").append("<option value='Nursing'> Nursing </option>");
+        } else if ($("select#fac2 option:selected").val() == "YSTCM") {
+            $("select#maj2 option").remove();
+            $("select#maj2").append("<option value=> Select Major </option>");
+            $("select#maj2").append("<option value='Performance'> Performance </option>");
+            $("select#maj2").append("<option value='Composition'> Composition </option>");
         } else {
             $("select#maj2 option").remove();
-            $("select#maj2").append("<option value=> Select Major/Minor </option>");
+            $("select#maj2").append("<option value=> Select Major </option>");
         }
             
     })
@@ -159,7 +287,7 @@ $(document).ready(function(){
     })
 
     jQuery.validator.addMethod('Integer', function(value, element) {
-        return this.optional(element) || /^[0-9,]+$/i.test(value);
+        return this.optional(element) || /Chinese Language[0-9,]+$/i.test(value);
     }, "Pls enter an integer.")
 
     $("#reviewform").validate({
